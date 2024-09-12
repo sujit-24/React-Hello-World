f79166a31e8dce24f8ae2c4487c2612a42845352
sujit-24 <sujitnahir@gmail.com>
2024-09-13 00:29:28 +0530

    feat: addition

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App text-2xl text-emerald-500 container mx-auto p-2 bg-violet-200" onClick={()=>{
+      var a = 10;
+      var b=30;
+      var c = a+b;
+      console.log(c);
       console.log("Hello world");
     }}>
       <h1>Welcome to React</h1>
