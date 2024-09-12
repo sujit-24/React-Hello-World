8b25a92ec2fbd4103abc99528616dd5e962be630
sujit-24 <sujitnahir@gmail.com>
2024-09-13 00:23:30 +0530

    Changes done in home page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App text-2xl text-emerald-500 container mx-auto p-2 bg-violet-200" onClick={()=>{
+      console.log("Hello world");
+    }}>
       <h1>Welcome to React</h1>
     </div>
   );
